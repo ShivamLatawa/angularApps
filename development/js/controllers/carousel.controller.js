1f72bc610aa295f78c4562c862c8d6dc41f39324
Shivam Latawa <slatawa@sapient.com>
2015-03-22 15:34:53 +0530

    css_updated

--- development/js/controllers/carousel.controller.js
+++ development/js/controllers/carousel.controller.js
@@ -2,13 +2,16 @@
   $scope.myInterval = 3000;
   $scope.slides = [
     {
-      image: 'images/img1.jpg'
+      image: 'images/img1.jpg',
+      text:'Get your products serviced easily and quickly'
     },
     {
-      image: 'images/img2.jpg'
+      image: 'images/img2.jpg',
+      text :'Easy and convenient way'
     },
     {
-      image: 'images/img3.jpg'
+      image: 'images/img3.jpg',
+      text :'Door to door service'
     }
   ];
 });
